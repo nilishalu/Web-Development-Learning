fc30edac971ba8daea2ffdc42ffe6046c5902b7b
nilishalu <shalini.c.e.ganu@gmail.com>
2022-12-04 15:46:57 +0530

    Added signup page of newletter-project

---
+++ API/newsletter-signup/app.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
+
+const app = express();
+
+app.use(express.static("public"))
+app.use(bodyParser.urlencoded({extended: true}))
+
+app.post('/', function(req, res) {
+    var firstName = req.body.firstName;
+    var lastName = req.body.lastName;
+    var email = req.body.email;
+
+    console.log(firstName, lastName, email)
+})
+
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + "/signup.html");
+})
+
+app.listen(3000, function () {
+    console.log("Server running at port 3000");
+})
